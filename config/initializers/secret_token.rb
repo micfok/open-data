2498260eba60918424d8864aebb4d553c90e5d5e
Michael <michaelmfok@gmail.com>
2014-02-28 21:59:54 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenData::Application.config.secret_key_base = '85ae54b26e2ec49cfb69dc89735e1b0b38de345afe68b0b49717264419718659a63ce4182eb694c42c125733cf2fe58e8703eef768df06c54697d88999459fbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpenData::Application.config.session_store :cookie_store, key: '_open-data_session'
